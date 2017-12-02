76fdc64ca30b9de93936a6e746067b53ceb22ba7
Oliver Cordingley <>
2017-12-02 17:41:58 +0000

    Built the product page.

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,14 +2,25 @@
 
   namespace Cart\Controllers;
 
+  use Slim\Router;
   use Cart\Models\Product;
   use Slim\Views\Twig;
   use Psr\Http\Message\ResponseInterface as Response;
   use Psr\Http\Message\ServerRequestInterface as Request;
 
   class ProductController{
-    public function get($slug,Request $request,Response $response,Twig $view,Product $product){
-      return $view->render($response,'products/product.twig');
+
+    public function get($slug,Request $request,Response $response,Twig $view,Product $product,Router $router){
+      $product = $product->where('slug',$slug)->first();
+
+      if (!$product){
+        return $response->withRedirect($router->pathFor('home'));
+      }
+
+      return $view->render($response,'products/product.twig',[
+        'product' => $product,
+        'home' => $router->pathFor('home')
+      ]);
     }
   }
 ?>
