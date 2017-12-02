c5b2fbc68676db19bc8f0c08bfab6b43c954f3a8
Sam Reynolds <samuel@nicks.net>
2017-12-02 09:57:14 +0000

    Admin page changing

--- admin/admin.php
+++ admin/admin.php
@@ -1,5 +1,5 @@
 <?php
-  include('session.php');
+  // include('session.php');
 ?>
 
 <!DOCTYPE html>
@@ -29,12 +29,44 @@
           <div id="dashboard">
             <h2 class="header"><span class="icon"></span>Dashboard</h2>
             <div class="monitor">
-              <h4>Right Now</h4>
+              <h4>Current Overview</h4>
               <div class="clear">
                 <ul class="content">
                   <li>Content</li>
-                  <li class="purchases"><span class="count">573 </span><a href="#">Purchases</a></li>
-                  <li class="users"><span>581 </span><a href="#">Users</a></li>
+                  <li class="purchases"><span class="count">573 </span><a class="half-disabled">Purchases</a></li>
+                  <li class="users"><span>581 </span><a class="half-disabled">Users</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div id="purchases">
+            <h2 class="header"><span class="icon"></span>Purchases</h2>
+            <div class="monitor">
+              <h4>Recent Purchases</h4>
+              <div class="clear">
+                <ul class="content">
+                  <li>Content</li>
+                  <li class="purchases"><span class="count">User123</span><a class="half-disabled"> bought 2 codeTode(); mugs</a></li>
+                  <li class="purchases"><span class="count">testing</span><a class="half-disabled"> bought 5 codeTode(); shirts</a></li>
+                  <li class="purchases"><span class="count">_johnX_</span><a class="half-disabled"> bought 1 codeTode(); laptop sticker</a></li>
+                  <li class="purchases"><span class="count">gerald9</span><a class="half-disabled"> bought 2 codeTode(); shirts</a></li>
+                  <li class="purchases"><span class="count">coding3</span><a class="half-disabled"> bought 1 codeTode(); laptop sticker</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div id="users">
+            <h2 class="header"><span class="icon"></span>Users</h2>
+            <div class="monitor">
+              <h4>Recent Users</h4>
+              <div class="clear">
+                <ul class="content">
+                  <li>Content</li>
+                  <li class="users"><span class="count">User123</span><a class="half-disabled"> registered 1 day ago</a></li>
+                  <li class="users"><span class="count">testyay</span><a class="half-disabled"> registered 2 days ago</a></li>
+                  <li class="users"><span class="count">fakeacc</span><a class="half-disabled"> registered 4 days ago</a></li>
+                  <li class="users"><span class="count">gerald9</span><a class="half-disabled"> registered 5 days ago</a></li>
+                  <li class="users"><span class="count">cTfan12</span><a class="half-disabled"> registered 6 days ago</a></li>
                 </ul>
               </div>
             </div>
--- admin/js/display.js
+++ admin/js/display.js
@@ -1,11 +1,53 @@
 function displayDashboard() {
+  var dashboard = document.getElementById('dashboard');
+  var purchases = document.getElementById('purchases');
+  var users = document.getElementById('users');
 
+  dashboard.style.opacity = 100;
+  dashboard.style.width = 1000;
+  dashboard.style.height = 1000;
+
+  purchases.style.opacity = 0;
+  purchases.style.width = 0;
+  purchases.style.height = 0;
+
+  users.style.opacity = 0;
+  users.style.width = 0;
+  users.style.height = 0;
 }
 
 function displayPurchases() {
+  var dashboard = document.getElementById('dashboard');
+  var purchases = document.getElementById('purchases');
+  var users = document.getElementById('users');
+  
+  dashboard.style.opacity = 0;
+  dashboard.style.width = 0;
+  dashboard.style.height = 0;
 
+  purchases.style.opacity = 100;
+  purchases.style.width = 1000;
+  purchases.style.height = 1000;
+
+  users.style.opacity = 0;
+  users.style.width = 0;
+  users.style.height = 0;
 }
 
 function displayUsers() {
-  
+  var dashboard = document.getElementById('dashboard');
+  var purchases = document.getElementById('purchases');
+  var users = document.getElementById('users');
+
+  dashboard.style.opacity = 0;
+  dashboard.style.width = 0;
+  dashboard.style.height = 0;
+
+  purchases.style.opacity = 0;
+  purchases.style.width = 0;
+  purchases.style.height = 0;
+
+  users.style.opacity = 100;
+  users.style.width = 1000;
+  users.style.height = 1000;
 }
