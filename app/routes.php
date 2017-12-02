8fb06b6ebf70ac6c797dd3be7645f4f488f86685
Oliver Cordingley <>
2017-12-02 18:15:45 +0000

    Starting on the Cart window

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
   $app->get('/',['Cart\Controllers\HomeController','index'])->setName('home');
   $app->get('/products/{slug}', ['Cart\Controllers\ProductController', 'get'])->setName('product.get');
+  $app->get('/cart',['Cart\Controllers\CartController','index'])->setName('cart.index');
 ?>
