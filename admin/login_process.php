909703a17f5868558d5e45f1da8e7c1a38c0153f
Oliver Cordingley <cord.codes@gmail.com>
2017-12-02 18:29:47 +0000

    Cleared the PHP to be ready for later backend built.

--- admin/login_process.php
+++ admin/login_process.php
@@ -1,27 +1,3 @@
 <?php
-  session_start();
-  $error = '';
-  if(isset($_POST['submit'])) {
-    if(empty($_POST['username']) || empty($_POST['password'])) {
-      $error = "Incorrect username or password.";
-    } else {
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-      $connection = mysql_connect("localhost:6112", "root", "");
-      $username = stripslashes($username);
-      $password = stripslashes($password);
-      $username = mysql_real_escape_string($username);
-      $password = mysql_real_escape_string($password);
-      $db = mysql_select_db("admin", $connection);
-      $query = mysql_query("SELECT * FROM login WHERE password=$'password' AND username='username'.", $connection);
-      $rows = mysql_num_rows($query);
-      if($rows == 1) {
-        $_SESSION['login_user']=$username;
-        header("location: admin.php");
-      } else {
-        $error = "Incorrect username or password.";
-      }
-      mysql_close($connection);
-    }
-  }
+  
 ?>
